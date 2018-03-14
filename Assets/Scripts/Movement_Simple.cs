7676e487d1cd25191274693cbc1fdf4fac5d6e87
Retus001 <retus001@gmail.com>
2018-03-14 13:04:16 -0600

    Main Mechanics Test A

---
+++ Assets/Scripts/Movement_Simple.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement_Simple : MonoBehaviour {
+
+	public float vel;
+	Rigidbody2D rig;
+	public GameObject pellet;
+	public GameObject Aim;
+
+	// Use this for initialization
+	void Start () {
+		rig = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector2 rigVel = rig.velocity;
+		if (Input.GetKey (KeyCode.A)) 
+			rigVel.x = -vel;
+			
+		else if (Input.GetKey(KeyCode.D))
+			rigVel.x = vel;
+		else
+			rigVel.x = 0;
+			
+		if (Input.GetKey(KeyCode.W))
+			rigVel.y = vel;
+
+		else if (Input.GetKey(KeyCode.S))
+			rigVel.y = -vel;
+		else
+			rigVel.y = 0;
+
+		rig.velocity = rigVel;
+
+		if (Input.GetKey(KeyCode.E))
+			Aim.transform.Rotate (Vector3.forward * vel);
+		if (Input.GetKey(KeyCode.Q))
+			Aim.transform.Rotate (Vector3.back * vel);
+
+		if (Input.GetKeyDown (KeyCode.Space))
+			PoolManager.Spawn (pellet, transform.position, transform.rotation);
+
+	}
+}
---
+++ Assets/Scripts/Pool_Prefab.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pool_Prefab : MonoBehaviour {
+
+	void OnSpawn()
+	{
+		Invoke ("Suicide", 2);
+	}
+
+	void Suicide()
+	{
+		PoolManager.Despawn (gameObject);
+	}
+}
