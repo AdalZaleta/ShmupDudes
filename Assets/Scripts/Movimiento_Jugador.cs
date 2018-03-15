06518ce4a422648d27d5a8800fe368c5725aa622
AdalZaleta <32682010+AdalZaleta@users.noreply.github.com>
2018-03-15 12:33:02 -0600

    Movimiento Terminado
    EL morvimiento terminado

--- Assets/Scripts/Movimiento_Jugador.cs
+++ Assets/Scripts/Movimiento_Jugador.cs
@@ -6,14 +6,14 @@ public class Movimiento_Jugador : MonoBehaviour {
 
 	public float Xaxis;
 	public float XRaxis;
-	public float VelX;
 	public float Yaxis;
 	public float YRaxis;
-	public float VelY;
+	public float Velocidad;
+	public float AnguloR;
 
 	// Use this for initialization
 	void Start () {
-		
+		Velocidad = 4.3f;
 	}
 	
 	// Update is called once per frame
@@ -24,9 +24,17 @@ public class Movimiento_Jugador : MonoBehaviour {
 		XRaxis = Input.GetAxis ("RightStick H");
 		YRaxis = Input.GetAxis ("RightStick V");
 
-		gameObject.transform.Translate (Vector3.right * Time.deltaTime * Xaxis * VelX);
-		gameObject.transform.Translate (Vector3.forward * Time.deltaTime * Yaxis * VelY);
+		gameObject.transform.Translate (Vector3.right * Time.deltaTime * Xaxis * Velocidad, Space.World);
+		gameObject.transform.Translate (Vector3.forward * Time.deltaTime * Yaxis * Velocidad, Space.World);
 
-		gameObject.transform.Rotate ()
+		if (XRaxis != 0.0f || YRaxis != 0.0f) 
+		{
+			AnguloR = Mathf.Atan2 (YRaxis, XRaxis) * Mathf.Rad2Deg;
+		}
+		//transform.Rotate ( Vector3.up * AnguloR, Space.World);
+
+		Debug.Log (AnguloR);
+		transform.rotation = Quaternion.AngleAxis (AnguloR, Vector3.up);
+		//gameObject.transform.Rotate ();
 	}
 }
