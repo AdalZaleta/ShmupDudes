503b58982f4ca3319f250baa40788ce984e357f7
Retus001 <retus001@gmail.com>
2018-03-20 01:37:42 -0600

    Enemy AI
    Basic enemy movement (Nav Mesh)

---
+++ Assets/Scripts/Enemy_AI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy_AI : MonoBehaviour {
+
+	GameObject Player;
+	NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+		Player = GameObject.FindGameObjectWithTag ("Player");
+		agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.rotation = Quaternion.identity;
+		agent.destination = Player.transform.position; 
+	}
+}
