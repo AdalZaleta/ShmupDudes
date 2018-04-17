1666bbda1acab215dbd6f1ada90e88a48af87ed3
Retus001 <retus001@gmail.com>
2018-04-17 17:28:10 -0500

    In-Game Death Menu
    Reload Scene or Exit to Main Menu

--- Assets/_C/Scripts/YouDied.cs
+++ Assets/_C/Scripts/YouDied.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEditor.SceneManagement;
 
 public class YouDied : MonoBehaviour {
 
 	GameObject player;
 	public Image ded;
+	public Text txtA;
+	public Text txtB;
 	bool isded = false;
 	float alpha = 0;
 
@@ -23,6 +26,14 @@ public class YouDied : MonoBehaviour {
 				StartCoroutine (death (0));
 				isded = true;
 			}
+		if (isded && Input.anyKeyDown && !Input.GetKeyDown(KeyCode.Escape))
+		{
+			EditorSceneManager.LoadScene (EditorSceneManager.GetActiveScene().name);
+		}
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			EditorSceneManager.LoadScene ("UI_MainMenu");
+		}
 	}
 
 	IEnumerator death(float alph)
@@ -31,6 +42,8 @@ public class YouDied : MonoBehaviour {
 		{
 			alpha+=0.03f;
 			ded.color = new Color (1.000f, 1.000f, 1.000f, alpha);
+			txtA.color = new Color (1.000f, 1.000f, 1.000f, alpha);
+			txtB.color = new Color (1.000f, 1.000f, 1.000f, alpha);
 			yield return new WaitForSeconds (0.02f);
 			alph = alpha;
 			StartCoroutine (death (alph));
