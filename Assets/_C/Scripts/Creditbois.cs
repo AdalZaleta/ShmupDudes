2d3a32ea648f1930c34c30a153e405d32471b40f
Retus001 <retus001@gmail.com>
2018-04-18 12:43:42 -0500

    Credits
    Motion
    Placeholder Img

---
+++ Assets/_C/Scripts/Creditbois.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Creditbois : MonoBehaviour {
+
+	public GameObject piv;
+	public float vel;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		piv.transform.Translate (Vector3.up * vel);
+	}
+}
